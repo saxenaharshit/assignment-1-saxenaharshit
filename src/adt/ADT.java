861dd3fed7ae96e9af497394e12a635591f9fee2
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-23 23:02:29 +0530

    methods to (add/remove/search/viewAll) persons created.

--- src/adt/ADT.java
+++ src/adt/ADT.java
@@ -1,5 +1,9 @@
 package adt;
 
 public interface ADT<Person> {
+    public void add(Person data);
+    public void remove();
+    public void search(String firstName);
+    public void viewAll();
 
 }
