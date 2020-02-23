470ce4264101479f6632735003a7eaa047891fda
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-23 20:45:39 +0530

    person class created

--- src/helpingclasses/Person.java
+++ src/helpingclasses/Person.java
@@ -4,48 +4,8 @@ import java.util.ArrayList;
 
 public class Person {
 
-    private String firstName;
-    private String lastName;
-    private ArrayList<String> contactNumber;
-    private String email;
 
-    public String getFirstName(){
-        return firstName;
-    }
 
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
-    }
 
-    public String getLastName(){
-        return lastName;
-    }
 
-    public void setLastName(String lastName){
-        this.lastName = lastName;
-    }
-
-    public ArrayList<String> getContactNumber(){
-        return contactNumber;
-    }
-
-    public void setContactNumber(ArrayList<String> contactNumber){
-        this.contactNumber = contactNumber;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    @Override
-    public String toString(){
-        return "First Name:" + firstName + "\n" +
-                "Last Name:" + lastName + "\n" +
-                "Contact Number:" + contactNumber + "\n" +
-                "Email=" + email;
-    }
 }
