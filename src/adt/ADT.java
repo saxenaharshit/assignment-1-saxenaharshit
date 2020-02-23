29c09141fb53344b078e07fc7f8d8028a2d1a6aa
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-23 23:00:03 +0530

    interface 'ADT' created.

--- src/adt/ADT.java
+++ src/adt/ADT.java
@@ -1,8 +1,5 @@
 package adt;
 
 public interface ADT<Person> {
-    public void add(Person data);
-    public void remove();
-    public void search(String firstName);
-    public void viewAll();
+
 }
