a09ee3a1970d70472324a43c0bb5583630f75d57
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-24 22:01:14 +0530

    created a menu driven message.

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -8,6 +8,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ContBook contact = new ContBook();
-
+        while(true) {
+            ContManage maintainContact = new ContManage();
+            System.out.println("Welcome to DBC's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+        }
     }
 }
