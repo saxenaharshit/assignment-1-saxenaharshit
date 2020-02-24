a604b2d268999f8f93d50c1feeedf8a43ffaab7f
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-24 21:51:38 +0530

    add method created to take the first name and last name of the person.

--- src/defination/ContManage.java
+++ src/defination/ContManage.java
@@ -7,6 +7,31 @@ import java.util.Scanner;
 public class ContManage {
     Person person = new Person();
     Scanner sc = new Scanner( System.in );
+    public Person add(){
+        System.out.println( "You have chosen to add a new contact:" );
+        System.out.println( "Please enter the name of the person" );
+        System.out.print( "First Name:" );
+        person.setFirstName( sc.nextLine() );
+        System.out.print( "Last Name:" );
+        person.setLastName( sc.nextLine() );
+        ArrayList<String> number = new ArrayList<>();
+        while(true) {
+            System.out.print( "Contact Number:" );
+            number.add( sc.nextLine() );
+            System.out.print( "Would you like to add another contact number? (y/n):" );
+            if(sc.nextLine().equals( "n" ))
+                break;
+        }
+        person.setContactNumber( number );
+        System.out.print( "Would you like to add email address? (y/n): " );
+        if(sc.nextLine().equals( "y" )) {
+            System.out.print( "Email:" );
+            person.setEmail( sc.nextLine() );
+
+        }
+        return person;
+    }
+
 
 
 
