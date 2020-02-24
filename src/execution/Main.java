65abd77422b4bc15221c6a270e853d616ba18c50
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-24 21:58:38 +0530

    object of ContBook class made.

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ContBook contact = new ContBook();
 
     }
 }
