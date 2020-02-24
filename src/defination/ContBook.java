62ce840484f2d821001b86edb5f7b27f4691c457
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-24 20:08:18 +0530

    remove method and printName method created.

--- src/defination/ContBook.java
+++ src/defination/ContBook.java
@@ -37,7 +37,32 @@ public class ContBook implements ADT<Person> {
     }
 
     @Override
-    public void remove() {
+    public void remove(){
+        Node response = null;
+        if(head != null) {
+            Node<Person> temp = head;
+            Node previous = null;
+            int count = 0;
+            int index = printName();
+            sc.nextLine();
+            while(count < index - 1) {
+                previous = temp;
+                temp = temp.getNext();
+                count++;
+            }
+            System.out.println( temp.getData().getFirstName() + " " + temp.getData().getLastName() + "'s contact deleted from the list!" );
+
+            if(previous != null) {
+                response = previous.getNext();
+                previous.setNext( null );
+
+            }
+            else{
+                response = head;
+                head = null;
+            }
+
+        }
 
     }
 
@@ -49,6 +74,18 @@ public class ContBook implements ADT<Person> {
     @Override
     public void print() {
 
+    }
+
+    public int printName(){
+        System.out.println( "---Here are all your contacts---" );
+        Node<Person> temp = head;
+        int sNo = 1;
+        while(temp != null) {
+            System.out.println( sNo++ + ". " + temp.getData().getFirstName() + " " + temp.getData().getLastName() );
+            temp = temp.getNext();
+        }
+        System.out.print( "Press the number against the contact to delete it:" );
+        return sc.nextInt();
     }
 
 
