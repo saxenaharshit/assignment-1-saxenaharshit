cb294749a3da6565338b08b421ccbf50625fa381
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2020-02-24 20:30:48 +0530

    search method created.

--- src/defination/ContBook.java
+++ src/defination/ContBook.java
@@ -37,27 +37,26 @@ public class ContBook implements ADT<Person> {
     }
 
     @Override
-    public void remove(){
+    public void remove() {
         Node response = null;
-        if(head != null) {
+        if (head != null) {
             Node<Person> temp = head;
             Node previous = null;
             int count = 0;
             int index = printName();
             sc.nextLine();
-            while(count < index - 1) {
+            while (count < index - 1) {
                 previous = temp;
                 temp = temp.getNext();
                 count++;
             }
-            System.out.println( temp.getData().getFirstName() + " " + temp.getData().getLastName() + "'s contact deleted from the list!" );
+            System.out.println(temp.getData().getFirstName() + " " + temp.getData().getLastName() + "'s contact deleted from the list!");
 
-            if(previous != null) {
+            if (previous != null) {
                 response = previous.getNext();
-                previous.setNext( null );
+                previous.setNext(null);
 
-            }
-            else{
+            } else {
                 response = head;
                 head = null;
             }
@@ -67,14 +66,38 @@ public class ContBook implements ADT<Person> {
     }
 
     @Override
-    public void search(String firstName) {
-
-    }
-
-    @Override
     public void print() {
 
     }
+
+
+    @Override
+    public void search(String firstName){
+        Node<Person> temp = head;
+        int count = 0;
+        //to count the number of persons with the same name
+        while(temp != null) {
+            if(temp.getData().getFirstName().equals( firstName )) {
+                count++;
+            }
+            temp = temp.getNext();
+        }
+        if(count != 0) {
+            System.out.println( count + " match found!\n-------- * -------- * -------- * --------" );
+            temp = head;
+            while(temp != null) {
+                if(temp.getData().getFirstName().equals( firstName )) {
+                    System.out.println( temp.getData() );
+                    System.out.println( "-------- * -------- * -------- * --------" );
+                }
+                temp = temp.getNext();
+            }
+        }
+        else
+            System.out.println( "No Result Found." );
+    }
+
+
 
     public int printName(){
         System.out.println( "---Here are all your contacts---" );
